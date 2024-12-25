0c892d23aa6c79c5972f26144f8c8c65495f444c
aliyuprof100 <aliyuddeen95@gmail.com>
2024-12-26 00:22:48 +0100

    setting backend auth

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+
+
+// Initialize app and environment
+dotenv.config();
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to DevWorks!");
 });
 
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth", authRoutes);
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+//Connect MangoDB
+mongoose.connect("mongodb://localhost/devworks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
